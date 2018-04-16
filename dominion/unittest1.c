eb5b8992720e2480104f7469cfe3533c58fa8509
Aaron Berns <bernsa@flip1.engr.oregonstate.edu>
2018-04-16 15:16:30 -0700

    unittest1 100% coverage

--- dominion/unittest1.c
+++ dominion/unittest1.c
@@ -1,11 +1,33 @@
-/* unittest1.c : tests fullDeckCount() */
+/* unittest1.c : tests TestfullDeckCount() */
 
 #include "dominion.h"
 #include <stdio.h>
 #include "rngs.h"
 #include <stdlib.h>
 
-#define TESTFN "fullDeckCount()"
+#define TESTFN "TestfullDeckCount()"
+
+int TestfullDeckCount(int player, int card, struct gameState *state) {
+  int i;
+  int count = 0;
+
+  for (i = 0; i < state->deckCount[player]; i++)
+    {
+      if (state->deck[player][i] == card) count++;
+    }
+
+  for (i = 0; i < state->handCount[player]; i++)
+    {
+      if (state->hand[player][i] == card) count++;
+    }
+
+  for (i = 0; i < state->discardCount[player]; i++)
+    {
+      if (state->discard[player][i] == card) count++;
+    }
+
+  return count;
+}
 
 int main (int argc, char** argv) {
 
@@ -56,7 +78,7 @@ int main (int argc, char** argv) {
         G.discard[player1][i] = estate;
     }
 
-    int count = fullDeckCount(1, estate, &G);
+    int count = TestfullDeckCount(1, estate, &G);
     if (count == 13) {
         printf("passed\n");
     } else {
@@ -67,7 +89,7 @@ int main (int argc, char** argv) {
     // ----------- TEST 2: All decks do not contain card  --------------
 	printf("TEST 2: All three decks do not contain card \n");
 
-    count = fullDeckCount(1, province, &G);
+    count = TestfullDeckCount(1, province, &G);
     if (count == 0) {
         printf("passed\n");
     } else {
@@ -83,7 +105,7 @@ int main (int argc, char** argv) {
         G.discard[player1][i] = gold;
     }
 
-    count = fullDeckCount(1, gold, &G);
+    count = TestfullDeckCount(1, gold, &G);
     if (count == 5) {
         printf("passed\n");
     } else {
