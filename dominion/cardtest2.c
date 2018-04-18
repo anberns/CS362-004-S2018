110cc768e2049208f71c4cb613c43c54eefeb9ac
Aaron Berns <bernsa@flip3.engr.oregonstate.edu>
2018-04-18 10:16:30 -0700

    smithy test 1

--- dominion/cardtest2.c
+++ dominion/cardtest2.c
@@ -1,9 +1,8 @@
 /*
- * cardtest1.c : tests adventurer card
+ * cardtest2.c : tests smithy card
  *
  
  */
-
 
 #include "dominion.h"
 #include "dominion_helpers.h"
@@ -13,7 +12,7 @@
 #include "rngs.h"
 #include <stdlib.h>
 
-#define TESTCARD "adventurer"
+#define TESTCARD "Smithy"
 
 int main() {
     int passFlag = 1;
@@ -37,8 +36,9 @@ int main() {
 	initializeGame(numPlayers, k, seed, &G);
 
     //set player deck to desired contents for testing
-    G.handCount[thisPlayer] = 0;
-
+    G.handCount[thisPlayer] = 1;
+    G.hand[thisPlayer][0] = smithy;
+    
     G.deck[thisPlayer][4] = gold;
     G.deck[thisPlayer][3] = province; 
     G.deck[thisPlayer][2] = duchy;
@@ -47,29 +47,22 @@ int main() {
 
 	printf("----------------- Testing Card: %s ----------------\n", TESTCARD);
 
-	// ----------- TEST 1: Treasure cards in hand, others discarded --------------
-	printf("TEST 1: Treasure kept, others discarded\n");
+	// ----------- TEST 1: Three cards added --------------
+	printf("TEST 1: Three cards added\n");
     
-    //cardEffect(adventurer, choice1, choice2, choice3, &G, handpos, &bonus);
-    callAdventurer(&G, thisPlayer);
-    
-    // check hand, should have copper and silver
-    if (G.hand[thisPlayer][0] != gold || G.hand[thisPlayer][1] != silver) {
+    //cardEffect(smithy, choice1, choice2, choice3, &G, handpos, &bonus);
+    callSmithy(&G, thisPlayer, 0);
+
+    // check hand, should have gold, province, duchy
+    if (G.hand[thisPlayer][0] != duchy || G.hand[thisPlayer][1] != gold || G.hand[thisPlayer][2] != province) {
         passFlag = 0;
     }
-	printf("hand card = %d, expected = %d\n", G.hand[thisPlayer][0], gold);
-    printf("hand card = %d, expected = %d\n", G.hand[thisPlayer][1], silver);
+	printf("hand count = %d, expected = %d\n", G.handCount[thisPlayer], 3);
+	printf("hand card = %d, expected = %d\n", G.hand[thisPlayer][0], duchy);
+    printf("hand card = %d, expected = %d\n", G.hand[thisPlayer][1], gold);
+    printf("hand card = %d, expected = %d\n", G.hand[thisPlayer][2], province);
 
-    // check discard, should have estate and duchy
-    if (G.discard[thisPlayer][0] != duchy || G.discard[thisPlayer][1] != province) {
-        passFlag = 0;
-    }
-	printf("discard card = %d, expected = %d\n", G.discard[thisPlayer][0], duchy);
-    printf("discard card = %d, expected = %d\n", G.discard[thisPlayer][1], province);
-	// ----------- TEST 2: choice1 = 2 = +2 coins --------------
-	//printf("TEST 2: choice1 = 2 = +2 coins\n");
-
-	// ----------- TEST 2: Shuffle, treasure cards in hand, others discarded --------------
+   	/* ----------- TEST 2: Shuffle, treasure cards in hand, others discarded --------------
 	printf("TEST 2: Shuffle, treasure kept, others discarded\n");
 
     // initialize a game state and player cards
@@ -96,6 +89,7 @@ int main() {
             passFlag = 0;
         }
     }
+    */
 
     if (passFlag) {
 	    printf("\n >>>>> SUCCESS: Testing complete %s <<<<<\n\n", TESTCARD);
